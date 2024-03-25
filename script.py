c9e347a5eec8199c52a25e6951f1719e0fa2ab40
18372738 <133884450+18372738@users.noreply.github.com>
2024-03-26 00:26:49 +0300

    Added check for redirect

--- script.py
+++ script.py
@@ -4,14 +4,21 @@ import requests
 
 
 def catalog_books():
-    id = 0
-    for id in range(10):
-        id += 1
+    for id in range(1, 11):
         url = f"https://tululu.org/txt.php?id={id}"
         response = requests.get(url)
         response.raise_for_status()
-        with open(f"books/id{id}.txt", 'wb') as file:
-            file.write(response.content)
+        try:
+            check_for_redirect(response)
+            with open(f"books/id{id}.txt", 'wb') as file:
+                file.write(response.content)
+        except requests.exceptions.HTTPError:
+          print(f'Ссылка {url} не содержит книги для скачивания.')
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
 
 
 if __name__ == "__main__":
