191405f965ed3e6b7dcf0779b987f14117703a5e
18372738 <133884450+18372738@users.noreply.github.com>
2024-03-30 23:14:57 +0300

    download book covers

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urljoin, urlsplit, unquote
 
 import requests
 from bs4 import BeautifulSoup
@@ -7,22 +8,23 @@ from pathvalidate import sanitize_filename
 
 
 
-def catalog_books(folder='books/'):
+def get_catalog_books(number_book, folder='books/'):
+    os.makedirs(folder, exist_ok=True)
     for id in range(1, 11):
-        url = f"https://tululu.org/txt.php?id={id}"
+        url = f"https://tululu.org/txt.php?id={number_book}"
         response = requests.get(url)
         response.raise_for_status()
         try:
           check_for_redirect(response)
-          path = download_txt(id)
+          path = download_txt(number_book)
           with open(path, 'wb') as file:
               file.write(response.content)
         except requests.exceptions.HTTPError:
           print(f'Ссылка {url} не содержит книги для скачивания.')
 
 
-def get_author_and_title(book_number):
-    url = f'https://tululu.org/b{book_number}/'
+def get_author_and_title(number_book):
+    url = f'https://tululu.org/b{number_book}/'
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
@@ -32,20 +34,50 @@ def get_author_and_title(book_number):
     return author, title
 
 
-def download_txt(book_number, folder='books/'):
-    author, title = get_author_and_title(book_number)
+def download_txt(number_book, folder='books/'):
+    author, title = get_author_and_title(number_book)
     filename = f"{title}.txt"
     path = os.path.join(folder, filename)
     return(path)
+
 
 def check_for_redirect(response):
     if response.history:
         raise requests.exceptions.HTTPError
 
 
+def get_image_url(number_book):
+  url = f'https://tululu.org/b{number_book}/'
+  response = requests.get(url)
+  response.raise_for_status()
+  try:
+      check_for_redirect(response)
+      soup = BeautifulSoup(response.text, 'lxml')
+      path_image = soup.find('div', class_='bookimage').find('img')['src']
+      image_url = urljoin('https://tululu.org', path_image)
+      return image_url
+  except requests.exceptions.HTTPError:
+      return None
+
+
+def download_image(url, folder='images/'):
+    os.makedirs(folder, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    try:
+        check_for_redirect(response)
+        path = urlsplit(url)
+        filename = unquote(path.path).split('/')[-1]
+        path = os.path.join(folder, filename)
+        with open(path, 'wb') as file:
+            file.write(response.content)
+    except requests.exceptions.HTTPError:
+        return None
 
 
 if __name__ == "__main__":
-    os.makedirs('books', exist_ok=True)
-    catalog_books()
-   
+    for number_book in range(1, 11):
+        get_catalog_books(number_book)
+        url = get_image_url(number_book)
+        if url:
+            download_image(url)
